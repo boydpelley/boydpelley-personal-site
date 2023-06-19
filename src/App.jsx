388ed36eacdb86c818c2b404cad61f6660fb8b0f
boydpelley <boydjrpelley@gmail.com>
2023-06-19 16:12:07 -0300

    connect to amplify, start with function

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ function App() {
       <div className="bio">
         <h2>BIO</h2>
         <p>I'm an aspiring programmer from Sydney, Nova Scotia who loves the outdoors and the many thing that go with it. Some hobbies of mine include foraging, making homemade beer and cider, hiking, ice hockey and running.</p>
-        <p>Aside from my activity outdoors, one of my biggest passions is music. I play in a band, <a href="http://www.halfpastnineband.com">half past nine</a> as well as record and produce my own projects through FL Studio.</p>
-        <p></p>
+        <p>Aside from my activity outdoors, one of my biggest passions is music. I play guitar, bass, piano and sing; I play in a band, <a href="http://www.halfpastnineband.com">half past nine</a> as well as record and produce my own projects through FL Studio.</p>
+        <p>From the programming side, I work with React.js, HTML, CSS, Javascript, and Java, programming mainly web applications, which can be seen below in in the projects section.</p>
       </div>
     </>
   )
